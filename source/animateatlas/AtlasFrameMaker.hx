e355bc693b787e7d37e5a51d01194405267fdc4f
Randomxddd <amongus@sus.com>
2022-02-25 14:01:10 -0300

    fix

--- source/animateatlas/AtlasFrameMaker.hx
+++ source/animateatlas/AtlasFrameMaker.hx
@@ -13,13 +13,13 @@ import animateatlas.displayobject.SpriteMovieClip;
 import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxFramesCollection;
 import flixel.graphics.frames.FlxFrame;
-#if desktop || android
+//#if desktop || android
 import sys.FileSystem;
 import sys.io.File;
-#else
-import js.html.FileSystem;
-import js.html.File;
-#end
+//#else
+//import js.html.FileSystem;
+//import js.html.File;
+//#end
 
 using StringTools;
 class AtlasFrameMaker extends FlxFramesCollection
